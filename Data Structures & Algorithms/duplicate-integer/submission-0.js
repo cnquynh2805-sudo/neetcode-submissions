a28f97fcdf5ff6c176947a19b95cad99b65ecc8b
Queenie <cnquynh2805@gmail.com>
2026-05-14 11:37:00 +0700

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+  const set = new Set();
+
+  for (const num of nums) {
+
+    // duplicate
+    if (set.has(num)) {
+      return true;
+    }
+
+    set.add(num);
+  }
+
+  return false;
+}
+}
