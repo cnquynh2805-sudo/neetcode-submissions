33836b6a299708d377de59c2d3eadc706a3fcd98
Queenie <cnquynh2805@gmail.com>
2026-05-15 12:06:44 +0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+
+    const mid =
+      Math.floor((left + right) / 2);
+
+    // found
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    // target lớn hơn
+    if (nums[mid] < target) {
+      left = mid + 1;
+    }
+
+    // target nhỏ hơn
+    else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+}
