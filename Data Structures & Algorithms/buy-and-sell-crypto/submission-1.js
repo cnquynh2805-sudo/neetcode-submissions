e6b648de6081e6243dff5ebfbcb728ccd442bdd1
Queenie <cnquynh2805@gmail.com>
2026-05-19 23:37:32 +0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+
+        let l = 0;
+        let r = 1;
+        let best = 0;
+
+        while (r < prices.length) {
+
+            if (prices[l] < prices[r]) {
+
+                let profit = prices[r] - prices[l];
+
+                best = Math.max(best, profit);
+
+            } else {
+
+                l = r;
+            }
+
+            r++;
+        }
+
+        return best;
+    }
+}
