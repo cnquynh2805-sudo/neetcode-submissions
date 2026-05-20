554ccdecb1b732ff6cb2b654f6185d109e428f28
Queenie <cnquynh2805@gmail.com>
2026-05-20 14:33:52 +0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+    let stack = [];
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        }
+
+        else {
+
+            if (stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+}
+
